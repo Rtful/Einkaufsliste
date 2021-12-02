fc7c97c41c8ba0f96f33413036a9460f4d642ea4
Aaron Holenstein <4aron.dodger@gmail.com>
2021-12-02 16:51:42 +0100

    style

--- einkaufsliste/src/Einkaufsliste.js
+++ einkaufsliste/src/Einkaufsliste.js
@@ -3,7 +3,6 @@ import './Einkaufsliste.css';
 import {InputGroup} from 'react-bootstrap';
 import {FormControl} from "react-bootstrap";
 import {Form} from 'react-bootstrap';
-import DateTimePicker from 'react-datetime-picker';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -47,31 +46,33 @@ export default function Einkaufsliste() {
 
     return (
         <div className="App">
-            <h1>Deine Einkaufsliste</h1>
             <header className="App-header">
-                <input type="text" onChange={handleChange}/>
-                <button onClick={handleAdd}>+ add item</button>
-                <ul>
-                    {list.map((item) => (
-                        <li key={item.id}>{item.name}
-                            <div>
-                                {['checkbox'].map((type) => (
-                                    <div key={`default-${type}`} className="mb-3">
-                                        <Form.Check
-                                            type={type}
-                                            id={`default-${type}`}
-                                            label={`default ${type}`}
-                                        />
-                                    </div>
-                                ))}
-                            </div>
-                            <button type="button" onClick={() => handleRemove(item.id)}>
-                                Remove
-                            </button>
-                        </li>
-                    ))}
-                </ul>
+                <h1>Deine Einkaufsliste</h1>
             </header>
+<div id="addContainer">
+            <input className="listInput" type="text" onChange={handleChange}/>
+            <button className="addButton" onClick={handleAdd}></button>
+</div>
+            <ul>
+                {list.map((item) => (
+                    <li key={item.id}>{item.name}
+                        <div>
+                            {['checkbox'].map((type) => (
+                                <div key={`default-${type}`} className="mb-3">
+                                    <Form.Check
+                                        type={type}
+                                        id={`default-${type}`}
+                                        label={`default ${type}`}
+                                    />
+                                </div>
+                            ))}
+                        </div>
+                        <button type="button" onClick={() => handleRemove(item.id)}>
+                            Remove
+                        </button>
+                    </li>
+                ))}
+            </ul>
         </div>
     );
 
