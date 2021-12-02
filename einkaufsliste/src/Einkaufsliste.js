b50524030a413286541c9342dc679afd396a4048
Aaron Holenstein <4aron.dodger@gmail.com>
2021-12-02 16:12:17 +0100

    <3

--- einkaufsliste/src/Einkaufsliste.js
+++ einkaufsliste/src/Einkaufsliste.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import './Einkaufsliste.css';
 import {InputGroup} from 'react-bootstrap';
 import {FormControl} from "react-bootstrap";
@@ -9,62 +9,70 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function Einkaufsliste() {
 
-    const [name, setName] = useState(0)
-    const [date, setDate] = useState(0)
-    const [phonenumber, setPhonenumber] = useState("")
-    const [email, setEmail] = useState("")
-    const [numberOfGuests, setNumberOfGuests] = useState(1)
-    const [address, setAddress] = useState(1)
+    const [idCounter, setIdCounter] = useState(0)
+    const initialList = [];
+    const [list, setList] = useState(initialList);
+    const [product, setProduct] = useState('');
 
-    const [value, onChange] = useState(new Date());
+    function handleChange(event) {
+        setProduct(event.target.value);
+    }
 
+    function handleAdd() {
+        setIdCounter(idCounter + 1)
+        const newList = list.concat({name: product, id: idCounter});
+        setList(newList);
+    }
 
-    return ( // JSX
-        <div>
-            <h1>Tischreservation</h1>
-            <div id="formContainer">
-                <>
-                    <Form.Label htmlFor="basic-url">Persönliche Angaben</Form.Label>
+    function handleRemove(id) {
+        const newList = list.filter((item) => item.id !== id);
+        setList(newList);
+    }
 
-                    <InputGroup className="mb-3">
-                        <InputGroup.Text id="basic-addon1">Name/ Vorname</InputGroup.Text>
-                        <FormControl
-                            placeholder="Username"
-                            aria-label="Username"
-                            aria-describedby="basic-addon1"
+    function ShoppingListElement(text) {
+        return (
+            <div>
+                {['checkbox'].map((type) => (
+                    <div key={`default-${type}`} className="mb-3">
+                        <Form.Check
+                            type={type}
+                            id={`default-${type}`}
+                            label={`default ${type}`}
                         />
-                    </InputGroup>
-
-                    <InputGroup className="mb-3">
-                        <InputGroup.Text id="basic-addon2">Adresse</InputGroup.Text>
-                        <FormControl
-                            placeholder="Recipient's username"
-                            aria-label="Recipient's username"
-                            aria-describedby="basic-addon2"
-                        />
-                    </InputGroup>
-
-                    <InputGroup className="mb-3">
-                        <InputGroup.Text id="basic-addon3">
-                            Telefonnummer
-                        </InputGroup.Text>
-                        <FormControl id="basic-url" aria-describedby="basic-addon3"/>
-                    </InputGroup>
-
-                    <InputGroup className="mb-3">
-                        <InputGroup.Text id="basic-addon3">
-                            E-Mail Adresse
-                        </InputGroup.Text>
-                        <FormControl id="basic-url" aria-describedby="basic-addon3"/>
-                    </InputGroup>
-
-                    <InputGroup className="dateTimePicker mb-3">
-                        <InputGroup.Text>Datum und Zeit auswählen</InputGroup.Text>
-                        <FormControl className="datePicker" type="Date"/>
-                        <FormControl className="timePicker" type="Time"/>
-                    </InputGroup>
-                </>
+                    </div>
+                ))}
             </div>
+        )
+    }
+
+    return (
+        <div className="App">
+            <h1>Deine Einkaufsliste</h1>
+            <header className="App-header">
+                <input type="text" onChange={handleChange}/>
+                <button onClick={handleAdd}>+ add item</button>
+                <ul>
+                    {list.map((item) => (
+                        <li key={item.id}>{item.name}
+                            <div>
+                                {['checkbox'].map((type) => (
+                                    <div key={`default-${type}`} className="mb-3">
+                                        <Form.Check
+                                            type={type}
+                                            id={`default-${type}`}
+                                            label={`default ${type}`}
+                                        />
+                                    </div>
+                                ))}
+                            </div>
+                            <button type="button" onClick={() => handleRemove(item.id)}>
+                                Remove
+                            </button>
+                        </li>
+                    ))}
+                </ul>
+            </header>
         </div>
-    )
+    );
+
 }
